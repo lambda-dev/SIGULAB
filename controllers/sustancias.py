9fcdf09dca0009b4eb3a4bf96f5e69095317c760
arnaldoxpm <arnaldoquintero96@gmail.com>
2017-02-07 14:33:16 -0400

    Inventario y Bitácora Listo
    Faltan restricciones al agregar a las tablas

--- controllers/sustancias.py
+++ controllers/sustancias.py
@@ -18,14 +18,12 @@ def validar_bitacora(form):
 
 
 def insert_bitacora(form):
-    #db(db.t_inventario.id == 1).update(f_cantidadusointerno = 1)
     espF = request.vars['esp']
     sust = request.vars['sust']
     new  = float(str(db((db.t_bitacora.f_sustancia == sust)&(db.t_bitacora.f_espaciofisico == espF)).select(db.t_bitacora.f_cantidad).last())[20:-2])
     row = db((db.t_inventario.f_espaciofisico == espF)&(db.t_inventario.f_sustancia == sust)).select().first()
     row.update_record(f_cantidadusointerno=new)
     row.update_record(f_total = row.f_cantidadusointerno+row.f_cantidadonacion)
-    #row.update_record(f_seccion = long(str(db(db.t_espaciofisico.id == espF).select(db.t_espaciofisico.f_seccion))[26:]))
 
 @auth.requires_login()
 def sustanciapeligrosa_manage():
@@ -39,20 +37,26 @@ def sustanciapeligrosa_manage():
 @auth.requires_login()
 def select_inventario():
     espacios=False
-    lab=False
+    labs=False
     secciones=False
-    asd = db(db.t_laboratorio.f_jefe == 5).select().first()
-    if (auth.has_membership('Técnico')):
-        espacios = db(db.t_espaciofisico.f_tecnico == auth.user.id).select(db.t_espaciofisico.f_espacio, db.t_espaciofisico.id)
+
+    #si hay espacios fisicos
+    if (auth.has_membership('Jefe de Laboratorio') ):
+        espacios = db((db.t_laboratorio.f_jefe == auth.user.id)&(db.t_laboratorio.f_nombre == db.t_inventario.f_laboratorio)&(db.t_espaciofisico.id == db.t_inventario.f_espaciofisico)).select(db.t_espaciofisico.ALL,distinct=db.t_espaciofisico.id)
     elif (auth.has_membership('Jefe de Sección') ):
         espacios = db((db.t_espaciofisico.f_seccion == db.t_seccion.id)&(db.t_seccion.f_jefe == auth.user.id)).select(db.t_espaciofisico.f_espacio,db.t_espaciofisico.id)
-    elif (auth.has_membership('Jefe de Laboratorio') ):
-        espacios = db((db.t_laboratorio.f_jefe == auth.user.id)&(db.t_laboratorio.f_nombre == db.t_inventario.f_laboratorio)&(db.t_espaciofisico.id == db.t_inventario.f_espaciofisico)).select(db.t_espaciofisico.f_espacio,db.t_espaciofisico.id)
     else:
         espacios = db(db.t_espaciofisico.f_tecnico == auth.user.id).select(db.t_espaciofisico.f_espacio, db.t_espaciofisico.id)
 
-    if auth.has_membership('Jefe de Sección'):
+    #si tiene secciones
+    if auth.has_membership('Jefe de Laboratorio'):
+        secciones = db((db.t_laboratorio.f_jefe == auth.user.id)&(db.t_inventario.f_laboratorio == db.t_laboratorio.f_nombre)&(db.t_seccion.id == db.t_inventario.f_seccion)).select(db.t_seccion.ALL,distinct=db.t_seccion.id)
+    elif auth.has_membership('Jefe de Sección'):
         secciones = db(db.t_seccion.f_jefe == auth.user.id ).select(db.t_seccion.ALL)
+
+    #si tiene labs
+    if auth.has_membership('Jefe de Laboratorio'):
+        labs = db( (db.t_laboratorio.f_jefe == auth.user.id) ).select(db.t_laboratorio.ALL)
     return locals()
 
 
@@ -64,22 +68,24 @@ def inventario_manage():
     query = db.t_inventario.f_espaciofisico == espF
     db.t_inventario.f_espaciofisico.default = espF
     if (request.vars['lab']):
-        pass
+        lab = str(db(db.t_laboratorio.id == request.vars['lab']).select(db.t_laboratorio.f_nombre))[24:-2]
+        query = db.t_inventario.f_laboratorio == lab
+        db.t_inventario.f_espaciofisico.readable = True
+        db.t_inventario.f_seccion.readable = True
+        table = SQLFORM.smartgrid(db.t_inventario,constraints=dict(t_inventario=query),editable=False,
+        orderby=[db.t_inventario.f_espaciofisico,db.t_inventario.f_sustancia],create=False,csv=False,deletable=False,links_in_grid=False)
 
     if (request.vars['secc']):
         db.t_inventario.f_espaciofisico.readable = True
         query = (db.t_inventario.f_seccion == request.vars['secc'])
         table = SQLFORM.smartgrid(db.t_inventario,constraints=dict(t_inventario=query),onupdate=auth.archive,editable=False,
-        orderby=[db.t_inventario.f_espaciofisico,db.t_inventario.f_sustancia],create=False,csv=False,deletable=False,links_in_grid=False)#left=left)
+        orderby=[db.t_inventario.f_espaciofisico,db.t_inventario.f_sustancia],create=False,csv=False,deletable=False,links_in_grid=False)
         return locals()
-        #left= (db.t_espaciofisico.on(db.t_inventario.f_espaciofisico == db.t_espaciofisico.id) )
-        #query = ((  == db.t_inventario.f_espaciofisico)&(db.t_espaciofisico.f_seccion == db.t_seccion.id)&(db.t_seccion.f_jefe == auth.user.id))
 
-    if(auth.has_permission('tecnico','t_inventario')):
-        table = SQLFORM.smartgrid(db.t_inventario,constraints=dict(t_inventario=query),create=False,links_in_grid=False,csv=False,editable=False,deletable=False,details=False)
-    else:
-        table = SQLFORM.smartgrid(db.t_inventario,constraints=dict(t_inventario=query),onupdate=auth.archive)#left=left)
-        #table = SQLFORM.smartgrid(db.t_inventario,db.t_espaciofisico)
+    #if(auth.has_membership('tecnico','t_inventario')):
+    table = SQLFORM.smartgrid(db.t_inventario,constraints=dict(t_inventario=query),create=False,links_in_grid=False,csv=False,editable=False,deletable=False)
+    #else:
+    #    table = SQLFORM.smartgrid(db.t_inventario,constraints=dict(t_inventario=query),onupdate=auth.archive)
     return locals()
 
 @auth.requires_login()
